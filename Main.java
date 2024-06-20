ea282389082421bc4eea1a385148e10a8a8dad20
tabe yuuichi <emg6580@gmail.com>
2024-06-20 21:54:40 +0900

    commit

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.println("SEMonster!");
-    Monster monster = new Monster();
-    System.out.println(monster.toString());
+    System.out.println("SEMONSTER GAME\nShow a new monster!");
+    Player player = new Player();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
